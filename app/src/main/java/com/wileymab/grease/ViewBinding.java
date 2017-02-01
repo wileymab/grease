47052db4bdd39696ca8628e216ab84325bdc62d6
Matthew Wiley <wileymab@gmail.com>
2017-02-01 14:25:54 -0500

    Initial commit.

---
+++ app/src/main/java/com/wileymab/grease/ViewBinding.java
@@ -0,0 +1,15 @@
+package com.wileymab.grease;
+
+import android.view.View;
+
+/**
+ * Created by matt on 2/1/17.
+ */
+
+public class ViewBinding {
+
+    public static <T extends View> T getView() {
+        return null;
+    }
+
+}
